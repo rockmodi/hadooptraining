ea0eda5bd42287c08d0691b7942855606636d85a
rockmodi <rakesh.modi1585@gmail.com>
2017-12-28 21:39:51 +0530

    sql group by program

--- src/main/java/com/sqlprogram/count/Count.java
+++ src/main/java/com/sqlprogram/count/Count.java
@@ -1,21 +1,16 @@
 package com.sqlprogram.count;
 
 import java.io.IOException;
-import java.util.StringTokenizer;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
-import org.apache.hadoop.mapreduce.lib.input.KeyValueTextInputFormat;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
-import org.apache.hadoop.mapreduce.Reducer.Context;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-
 
 public class Count {
 	
@@ -55,6 +50,7 @@ public class Count {
 		}
 		
 	}
+	
 	
 	public static void main(String args[]) throws Exception{
 		
